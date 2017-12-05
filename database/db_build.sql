8729916af33674ca58b763d69c9250f58e838561
“Sophia” <jungwon.lim.0210@gmail.com>
2017-12-05 14:44:50 +0200

    wrote database skeleton

---
+++ database/db_build.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS gifts (
+  gift_id SERIAL PRIMARY KEY,
+  item TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS relationship (
+  rela_id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  gift_id INTEGER NOT NULL,
+  reserved BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS reservation (
+  res_id SERIAL PRIMARY KEY,
+  gift_id INTEGER NOT NULL,
+  donor_id INTEGER NOT NULL
+);
+
+
+
+
+COMMIT;
