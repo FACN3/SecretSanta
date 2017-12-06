897adbfd96c4a6a74aff8d2457976892f0efb82a
“Sophia” <jungwon.lim.0210@gmail.com>
2017-12-07 00:29:11 +0200

    finished generate.js function Relates 34

--- public/js/generate.js
+++ public/js/generate.js
@@ -2,6 +2,7 @@ function fetch(url, data, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr.responseText);
       callback(null, JSON.parse(xhr.responseText));
     } else if (xhr.readyState === 4 && xhr.status !== 200){
       callback(xhr.status);
@@ -15,16 +16,26 @@ function fetch(url, data, callback) {
 }
 
 document.getElementById('generate').addEventListener('click', function () {
-  var first_name = document.getElementById('first_name').value;
-  var last_name = document.getElementById('last_name').value;
+  var first_name = document.getElementById('first_name').value.toLowerCase();
+  var last_name = document.getElementById('last_name').value.toLowerCase();
   var data = [first_name, last_name];
   console.log('this is data', data);
   fetch('/generate', data, function (err, res) {
     if(err) {
       console.log('there was an error', err);
     } else {
-      console.log('response',res);
-      // alert('information sent');
+      console.log('THE response', res);
+      showShoppingList(res);
     }
   });
 });
+
+function showShoppingList(arr) {
+    var shopList = document.createElement('ul');
+    arr.forEach(function(item){
+      var listElement = document.createElement('li');
+      listElement.textContent = item.item;
+      shopList.appendChild(listElement);
+    });
+    document.getElementById('shopping_list').appendChild(shopList);
+  }
