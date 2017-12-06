07f128a5e03fcf96e0ccab50aabaeddf54479973
MynahMarie <mynah.marie@gmail.com>
2017-12-06 14:13:25 +0200

    Create the first draft of css
    Relates #19

---
+++ src/handleStatic.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const handleError = require('./handleError');
+
+const handleStatic = (req, res) => {
+  const url = req.url;
+  const ext = req.url.split('.')[1];
+  const types = {
+    css: 'text/css',
+    html: 'text/html',
+    js: 'application/javascript',
+    ico: 'image/x-icon'
+  }[ext];
+
+  let filePath;
+  if (ext === 'js') {
+    filePath = path.join(__dirname, '..', 'public', 'js', url);
+  } else if (ext === 'ico'){
+    filePath = path.join(__dirname, '..', 'public', 'assets', url);
+  } else {
+    filePath = path.join(__dirname, '..', 'public', url);
+  }
+
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      handleError(err, req, res);
+    } else {
+      res.writeHead(200, {'Content-Type' : types });
+      res.end(file);
+    }
+  })
+}
+
+module.exports = handleStatic;
