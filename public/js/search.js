9306f142134263f9e62ca05df6db38212c5354d4
hasansaad1 <hasansaad.saad1998@gmail.com>
2017-12-07 12:43:36 +0200

    styled the list result in the search page
    relates #65

--- public/js/search.js
+++ public/js/search.js
@@ -13,22 +13,26 @@ function renderingSearchResult(err, arr) {
     var idx = 1;
     document.getElementById('resDiv').innerHTML = '';
     var wishes = [];
+    var results=document.createElement('div');
+    results.setAttribute('id', 'results');
     arr.forEach(function(gift) {
-      if (gift.gifts !== "" && wishes.indexOf(gift.gifts) === -1) {
+      if (gift.gifts !== "" && wishes.indexOf(gift.gifts) === -1 && gift.reserved == false) {
         wishes.push(gift.gifts);
-          var label = document.createElement('label');
-          var checkbox = document.createElement('input');
-          var description = document.createTextNode(gift.gifts);
-          checkbox.type = 'checkbox';
-          checkbox.name = gift.rela_id;
-          checkbox.setAttribute('id', 'gift' + idx.toString());
-          checkbox.value = gift.gifts;
-          label.appendChild(checkbox);
-          label.appendChild(description);
-          document.getElementById('resDiv').appendChild(label);
-          idx++;
-        }
+        var label = document.createElement('label');
+        var checkbox = document.createElement('input');
+        var description = document.createTextNode(gift.gifts);
+        checkbox.type = 'checkbox';
+        checkbox.name = gift.rela_id;
+        checkbox.setAttribute('id', 'gift' + idx.toString());
+        checkbox.value = gift.gifts;
+        checkbox.classList.add('checkbox--gifts');
+        label.appendChild(checkbox);
+        label.appendChild(description);
+        results.appendChild(label);
+        idx++;
+      }
     });
+    document.getElementById('resDiv').appendChild(results);
 
     // If the wishlist is empty, return appropriate message to user.
     var count = document.getElementsByTagName('label').length;
@@ -40,45 +44,47 @@ function renderingSearchResult(err, arr) {
       // Otherwise, create the wishlist with an input form (for the name of the donor)
       // and a submit button.
     } else {
-        var donor = document.createElement('input');
-        donor.type = 'text';
-        donor.setAttribute('id', 'donor');
-        donor.placeholder = 'Your name';
-        document.getElementById('resDiv').appendChild(donor);
+      var donor = document.createElement('input');
+      donor.type = 'text';
+      donor.setAttribute('id', 'donor');
+      donor.placeholder = 'Your name';
+      donor.classList.add('input--donor');
+      document.getElementById('results').appendChild(donor);
 
-        var submit = document.createElement('button');
-        submit.textContent = "Submit";
-        submit.setAttribute('id', 'reserve');
-        document.getElementById('resDiv').appendChild(submit);
+      var submit = document.createElement('button');
+      submit.textContent = "Submit";
+      submit.setAttribute('id', 'reserve');
+      submit.classList.add('button--reserve')
+      document.getElementById('results').appendChild(submit);
 
-        // If submit button is clicked, submit the appropriate data to the database
-        // to make the reservation.
-        document.getElementById('reserve').addEventListener('click', function() {
-          var inputs = document.getElementsByTagName('input');
-          var idx = 1;
-          var resList = [];
-          for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i].type === 'checkbox') {
-              if (inputs[i].checked === true) {
-                resList.push(parseInt(inputs[i].name, 10));
-              }
+      // If submit button is clicked, submit the appropriate data to the database
+      // to make the reservation.
+      document.getElementById('reserve').addEventListener('click', function() {
+        var inputs = document.getElementsByTagName('input');
+        var idx = 1;
+        var resList = [];
+        for (var i = 0; i < inputs.length; i++) {
+          if (inputs[i].type === 'checkbox') {
+            if (inputs[i].checked === true) {
+              resList.push(parseInt(inputs[i].name, 10));
             }
           }
-          var fullName = document.getElementById('donor').value;
-          var first_name = fullName.split(' ')[0].toLowerCase();
-          var last_name = fullName.split(' ')[1].toLowerCase();
-          var data = [first_name, last_name, resList];
+        }
+        var fullName = document.getElementById('donor').value;
+        var first_name = fullName.split(' ')[0].toLowerCase();
+        var last_name = fullName.split(' ')[1].toLowerCase();
+        var data = [first_name, last_name, resList];
 
-          postSearch('/reserve', data, function(err, res) {
-            if (err) {
-              console.log(err);
-            } else {
-              // On success, aler the user that the reservation has been submitted.
-              alert(res);
-            }
-          })
-        });
-      }
+        postSearch('/reserve', data, function(err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            // On success, aler the user that the reservation has been submitted.
+            alert(res);
+          }
+        })
+      });
+    }
   }
 }
 
